985f52b04b83dcfc91abe60d7cdb987a3ece1fb2
Jaco <137519562+Jaco-dr@users.noreply.github.com>
2025-04-02 11:24:56 +0200

    Create adressen.js

---
+++ adressen.js
@@ -0,0 +1,13 @@
+// Adressen voor wijk 1
+const wijk1Data = [
+    { "name": "Jan Jansen", "address": "Dorpsstraat 1" },
+    { "name": "Piet Pietersen", "address": "Kerklaan 12" }
+];
+
+// Adressen voor wijk 2
+const wijk2Data = [
+    { "name": "Mw. van de Broek", "address": "Vermeerlaan 1" },
+    { "name": "Wilmar Hardeman", "address": "Glashorst 46" },
+    { "name": "Fam. Wisse", "address": "Glashorst 68" }
+    // Voeg hier meer adressen toe als nodig
+];
